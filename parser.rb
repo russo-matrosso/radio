7976c70c4b8149352b91ff39a0ac5ecbf28f31f1
Anatoliy Kurichev <anatoliy.kurichev@rorqual.ru>
2016-02-22 02:17:01 +0500

    Refactored parser.rb via rubocop

--- parser.rb
+++ parser.rb
@@ -5,34 +5,25 @@ require 'nokogiri'
 class Song
   attr_accessor :title, :artist
 
-
   def parse_source(source)
-      source_string = Nokogiri::HTML(open(source)).text
-      self.title = source_string.scan(/\s-\s(.+?)$/).join
-      self.artist = source_string.scan(/^(.+?)\s-/).join
+    source_string = Nokogiri::HTML(open(source)).text
+    self.title = source_string.scan(/\s-\s(.+?)$/).join
+    self.artist = source_string.scan(/^(.+?)\s-/).join
   end
 
   def add_to_database
     return if self.exists_in_database?
     songs = DB.from(:songs)
 
-    songs.insert(title: self.title,
-                 artist: self.artist,
+    songs.insert(title: title,
+                 artist: artist,
                  added: Time.now)
-    puts "Song added, title: #{self.title}, artist: #{self.artist}"
+    puts "Song added, title: #{title}, artist: #{artist}"
   end
 
   def exists_in_database?
     songs = DB.from(:songs)
 
-    return !songs.where(title: self.title, artist: self.artist).empty?
+    !songs.where(title: title, artist: artist).empty?
   end
 end
-
-# source = 'http://37.58.75.166:8384/currentsong?sid=1'
-# song = Song.new
-# song.parse_source(source)
-# song.add_song
-#
-# puts song.inspect
-#
